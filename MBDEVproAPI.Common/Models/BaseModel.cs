66fde457dc61a7b6169129bbdb418eaa8847b07b
Mike Belcher <mikebdev@users.noreply.github.com>
2026-02-12 15:15:58 -0500

    .

---
+++ MBDEVproAPI.Common/Models/BaseModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MBDEVproAPI.Common.Models
+{
+    public class BaseModel
+    {
+
+        /// <summary>
+        /// Created By for entities
+        /// </summary>
+        [StringLength(50), Required]
+        public string CreatedBy { get; set; }
+
+        /// <summary>
+        /// Created Date for entities
+        /// </summary>
+        [Required]
+        public DateTime CreatedDate { get; set; }
+
+        /// <summary>
+        /// Modified By 
+        /// </summary>
+        [StringLength(50), Required]
+        public string ModifiedBy { get; set; }
+
+        /// <summary>
+        /// Modified Date
+        /// </summary>
+        [Required]
+        public DateTime ModifiedDate { get; set; }
+
+
+    }
+}
---
+++ MBDEVproAPI.Common/ViewModels/SaveViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MBDEVproAPI.Common.ViewModels
+{
+
+    public class SaveViewModel
+    {
+        /// <summary>
+        /// default constructor
+        /// </summary>
+        public SaveViewModel()
+        {
+            IsSaved = true;
+        }
+
+        /// <summary>
+        /// default constructor
+        /// <paramref name="refID">Ref ID of saved Record</paramref>
+        /// </summary>
+        public SaveViewModel(int? refID = null)
+        {
+            IsSaved = true;
+            RefID = refID;
+        }
+
+        /// <summary>
+        /// Error Constructor
+        /// </summary>
+        /// <param name="ErrMasg">Error Message</param>
+        public SaveViewModel(string errorMessage)
+        {
+            IsSaved = false;
+            ErrorMessage = errorMessage;
+        }
+
+        /// <summary>
+        /// Is record Saved
+        /// </summary>
+        public bool IsSaved { get; set; }
+
+        /// <summary>
+        /// Error Message
+        /// </summary>
+        public string ErrorMessage { get; set; }
+
+        /// <summary>
+        /// Saved Ref ID of Newly added record.
+        /// </summary>
+        public int? RefID { get; set; }
+    }
+
+}

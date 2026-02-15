5a944fe6815798d76376f9b64d26f959d8d4f14d
Mike Belcher <mikebdev@users.noreply.github.com>
2026-02-14 19:07:45 -0500

    3

--- MBDEVproAPI.DataModel/GlobalUsings.cs
+++ MBDEVproAPI.DataModel/GlobalUsings.cs
@@ -3,6 +3,7 @@ global using System.ComponentModel.DataAnnotations;
 global using System.ComponentModel.DataAnnotations.Schema;
 global using System.Linq;
 global using MBDEVproAPI.Common;
+global using MBDEVproAPI.Common.Models;
 //global using MBDEVproAPI.DataModel.Entities;
 global using Microsoft.AspNetCore.Http;
 global using Microsoft.EntityFrameworkCore;

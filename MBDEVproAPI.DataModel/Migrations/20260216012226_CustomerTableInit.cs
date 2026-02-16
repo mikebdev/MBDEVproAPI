c8e02481af9ab121d8c2d06e4102387d32d946b4
Mike Belcher <mikebdev@users.noreply.github.com>
2026-02-15 20:24:31 -0500

    another update 2

---
+++ MBDEVproAPI.DataModel/Migrations/20260216012226_CustomerTableInit.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MBDEVproAPI.DataModel.Migrations
+{
+    /// <inheritdoc />
+    public partial class CustomerTableInit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Company = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    FirstName = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(75)", maxLength: 75, nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    BPhone = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: true),
+                    HPhone = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: true),
+                    MPhone = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: true),
+                    Fax = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(75)", maxLength: 75, nullable: false),
+                    City = table.Column<string>(type: "nvarchar(75)", maxLength: 75, nullable: false),
+                    State = table.Column<string>(type: "nvarchar(75)", maxLength: 75, nullable: false),
+                    ZIPCode = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    WebPage = table.Column<string>(type: "nvarchar(75)", maxLength: 75, nullable: true),
+                    Notes = table.Column<string>(type: "nvarchar(max)", maxLength: 4010, nullable: true),
+                    Photo = table.Column<string>(type: "nvarchar(75)", maxLength: 75, nullable: true),
+                    Map = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
